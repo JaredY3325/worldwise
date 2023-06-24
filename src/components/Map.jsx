8ac2b4c8bb9ba2561efab471df56b4777ebc965c
Jerry Yu <JaredY3325@Gmail.com>
2023-06-24 17:17:05 -0600

    Refactored states with Context API and city data

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,13 +10,17 @@ function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
-        Change pos
-      </button>
+      {/* //   <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    //     <TileLayer */}
+      {/* //       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     />
+    //     <Marker position={[51.505, -0.09]}>
+    //       <Popup>
+    //         A pretty CSS3 popup. <br /> Easily customizable.
+    //       </Popup>
+    //     </Marker>
+    //   </MapContainer> */}
     </div>
   );
 }
